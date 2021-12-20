858b55cd540bd757a96dda26a42d406c9541e170
survivorr <kangur20040@wp.pl>
2021-12-21 00:55:30 +0100

    improved step detection
    - capsule cast extended in move direction instead of sphere cast thicker than player's collider
    - Move() function meant to replace CC's one
    - improved stability

--- Assets/Scripts/CCEnhanced.cs
+++ Assets/Scripts/CCEnhanced.cs
@@ -15,48 +15,20 @@ public class CCEnhanced : MonoBehaviour
     RaycastHit[] lastHits;
     RaycastHit[] hits;
     bool change;
+    public float stepOffset;
+    public float scanRadius;
+    public float scanForwardsStep;
+    public GameObject cube;
+    Vector3 lastMove;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        moveBuffer = new Vector3(Input.GetAxis("Horizontal"), isStepping ? 0 : -1f, Input.GetAxis("Vertical")) * 15 * Time.deltaTime;
-        player.Move(moveBuffer);
-        hits = Physics.SphereCastAll(transform.position + Vector3.up * player.height/2, player.radius * 2.5f, Vector3.down, player.height, ~layerMask);
-        if(lastHits != null)if(!hits.SequenceEqual(lastHits)){
-            change = true;
-        }else{
-            change = false;
-        }
-        if(change){
-            Hitpoints.Clear();
-            foreach(RaycastHit hit in hits){
-                Vector3 a = Vector3.Scale(moveBuffer.normalized, new Vector3(1, 0, 1)).normalized;
-                Vector3 b = Vector3.Scale(hit.point - transform.position, new Vector3(1, 0, 1)).normalized;
-                DrawVector(transform.position, a, 10, Color.yellow);
-                DrawVector(transform.position, b, 10, Color.green);
-                dot = Vector3.Dot(a, b);
-                if(dot > 0.9f){
-                    Hitpoints.Add(hit.point);
-                    DrawVector(hit.point, Vector3.up, 100, Color.black);
-                }
-            }
-        }
-
-            if(Hitpoints.Count > 0) 
-            {   foreach(Vector3 hit1 in Hitpoints){
-                dot = Vector3.Dot(Vector3.Cross(moveBuffer.normalized, new Vector3(1, 0, 1)), hit1 - transform.position);
-                Vector3 difference = Lerp3D(ref buffer, player.velocity.magnitude*3 * Time.deltaTime);
-                transform.position += difference;
-            }
-                isStepping = true;
-            }else{
-                isStepping = false;
-            }
+       Move(new Vector3(Input.GetAxis("Horizontal"), -1f, Input.GetAxis("Vertical")) * 5 * Time.deltaTime);
     }
     void LateUpdate(){
         lastHits = hits;
@@ -68,5 +40,44 @@ public class CCEnhanced : MonoBehaviour
         Vector3 difference = buffer - Vector3.Lerp(buffer, Vector3.zero, speed);
         buffer -= difference;
         return difference;
+    }
+    public void Move(Vector3 move){
+        float moveY = move.y;
+        if(isStepping)if(move.y < 0)moveY = 0;
+        moveBuffer = new Vector3(move.x, moveY, move.z);
+        player.Move(moveBuffer);
+        hits = Physics.CapsuleCastAll(transform.position + Vector3.up * player.height/2 - new Vector3(move.x, 0, move.z).normalized * scanForwardsStep, transform.position + Vector3.up * player.height/2 + new Vector3(move.x, 0, move.z).normalized * scanForwardsStep, player.radius * scanRadius, Vector3.down, player.height*2, ~layerMask);
+        if(lastHits != null)if(!hits.SequenceEqual(lastHits)){
+            change = true;
+        }else{
+            change = false;
+        }
+        if(change && moveBuffer.magnitude > 0){
+            Hitpoints.Clear();
+            foreach(RaycastHit hit in hits){
+                Vector3 a = Vector3.Scale(moveBuffer.normalized, new Vector3(1, 0, 1)).normalized;
+                Vector3 b = Vector3.Scale(hit.point - transform.position, new Vector3(1, 0, 1)).normalized;
+                DrawVector(transform.position, a, 10, Color.yellow);
+                DrawVector(transform.position, b, 10, Color.green);
+                DrawVector(hit.point, Vector3.up, 10, Color.cyan);
+                dot = Vector3.Dot(a, b);
+                if(dot > 0.9f && (1-(transform.position.y - hit.point.y)) <= stepOffset && (1-(transform.position.y - hit.point.y)) > 0){
+                    Hitpoints.Add(hit.point);
+                    DrawVector(hit.point, Vector3.up, 100, Color.black);
+                }
+            }
+        }
+            if(Hitpoints.Count > 0) 
+            {   
+                foreach(Vector3 hit1 in Hitpoints){
+                    dot = Vector3.Dot(Vector3.Scale(moveBuffer.normalized, new Vector3(1, 0, 1)).normalized, Vector3.Scale(hit1 - transform.position, new Vector3(1, 0, 1)).normalized);
+                    buffer = new Vector3(0, (1-(transform.position.y - hit1.y)), 0);
+                    Vector3 difference = Lerp3D(ref buffer, player.velocity.magnitude*3 * Time.deltaTime);
+                    transform.position += difference;
+                }
+                isStepping = true;
+            }else{
+                isStepping = false;
+            }
     }
 }
